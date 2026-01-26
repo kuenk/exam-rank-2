956003cc6d851b377edd86378acf54c731bda3c8
David Cuenca <111570446+kuenk@users.noreply.github.com>
2026-01-26 21:41:52 +0100

    Update 11.inter.c

--- lvl2/11.inter.c
+++ lvl2/11.inter.c
@@ -16,27 +16,39 @@ $
 */
 #include <unistd.h>
 
-int iter(char *str, char c, int len)
-{
-    int i = 0;
-    while(str[i] && (i< len || len == -1))
-    {
-        if (str[i++] == c)
-            return(1);
-    }
-    return(0);
-}
-
 int main(int argc, char **argv)
 {
     int i = 0;
+    int j = 0;
+    int k = 0;
+    char c;
+    
     if (argc == 3)
     {
         while(argv[1][i] != '\0')
         {
-            if(!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
-                write(1, &argv[1][i], 1);
-            i++;
+            c = argv[1][i];
+            k = 0;
+            while (k < i)
+                {
+                    if (argv[1][k] == c)
+                        break;
+                    k++;
+                }
+            if (k != i)
+                i++;
+
+            j = 0;
+            while(argv[2][j] != '\0')
+            {
+                    if (argv[2][j] == c)
+                    {
+                        write(1, &c, 1);
+                        break;
+                    }
+                    j++;
+            }
+            i++
         }
     }
     write(1, "\n", 1);
